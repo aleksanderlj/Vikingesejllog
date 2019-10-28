537aaa7cb7bb75e7679cd21eff59fb81a3f9bc83
aleksanderlj <aleksanderlj@hotmail.com>
2019-10-28 13:02:40 +0100

    Lavet ny tur/havn

---
+++ app/src/main/java/com/example/vikingesejllog/trip/NewTrip.java
@@ -0,0 +1,20 @@
+package com.example.vikingesejllog.trip;
+
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.vikingesejllog.R;
+
+public class NewTrip extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_trip);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
