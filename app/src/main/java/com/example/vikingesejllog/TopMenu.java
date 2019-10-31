7a6af0f0be99d10edb4e3ce22b9cc8d866762bb6
fmoltke <freggi64@live.dk>
2019-10-31 16:19:37 +0100

    lister UI lavet

---
+++ app/src/main/java/com/example/vikingesejllog/TopMenu.java
@@ -0,0 +1,19 @@
+package com.example.vikingesejllog;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class TopMenu extends Fragment {
+    public TopMenu(){
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_top, container, false);
+    }
+}
