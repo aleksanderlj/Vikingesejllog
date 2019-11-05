e865813701bff74abff35d8de5e1dd446731f38c
aleksanderlj <aleksanderlj@hotmail.com>
2019-11-05 10:23:22 +0100

    Fragment titel ændres når JourneyList starter

--- app/src/main/java/com/example/vikingesejllog/journey/JourneyList.java
+++ app/src/main/java/com/example/vikingesejllog/journey/JourneyList.java
@@ -2,11 +2,13 @@ package com.example.vikingesejllog.journey;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.vikingesejllog.R;
+import com.example.vikingesejllog.TopMenu;
 
 public class JourneyList extends AppCompatActivity implements View.OnClickListener {
 
@@ -15,6 +17,10 @@ public class JourneyList extends AppCompatActivity implements View.OnClickListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.journey_list);
+
+        TopMenu tm = (TopMenu) getSupportFragmentManager().findFragmentById(R.id.topMenuFragment);
+        tm.updateTextView("Liste over togter");
+
         findViewById(R.id.newHarborButton).setOnClickListener(this);
     }
 
