6a211d749bc7ca54a6305d9b8411f056b4cc95db
DovendyretS <afck97@hotmail.com>
2019-11-25 18:45:40 +0100

    implemented getting current coordinates

---
+++ app/src/main/java/com/example/vikingesejllog/MyGPS.java
@@ -0,0 +1,72 @@
+package com.example.vikingesejllog;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import androidx.core.content.ContextCompat;
+
+
+public class MyGPS implements LocationListener {
+
+    private Context mContext;
+
+    public MyGPS(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    public Location getLocation(){
+
+        if (ContextCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            return null;
+        }
+
+        LocationManager locationManager = (LocationManager)mContext.getSystemService(Context.LOCATION_SERVICE);
+        boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (isGPSEnabled){
+            locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER,this,null);
+            return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        }
+        return null;
+    }
+
+    public String getSpeed() {
+        if (ContextCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return null;
+        }
+
+        LocationManager locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+        boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (isGPSEnabled) {
+            locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, this, null);
+            return String.valueOf(locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getSpeed());
+
+        }
+        return null;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
