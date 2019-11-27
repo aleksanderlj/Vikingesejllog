acc0acb23c9da274eef34ac0469b8012773d267e
Hemsen <niko.hemsen@gmail.com>
2019-11-27 19:04:44 +0100

    Mere audio recording. Skal lave play nu

--- app/src/main/java/com/example/vikingesejllog/NoteRecorder.java
+++ app/src/main/java/com/example/vikingesejllog/NoteRecorder.java
@@ -1,16 +1,27 @@
 package com.example.vikingesejllog;
 
+import android.app.ProgressDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.media.MicrophoneDirection;
 import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 
+import androidx.core.content.FileProvider;
+
+import java.io.File;
 import java.io.IOException;
-import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static android.media.MediaRecorder.AudioEncoder.AAC;
+import static android.media.MediaRecorder.AudioEncoder.DEFAULT;
 
 public class NoteRecorder {
 
@@ -24,33 +35,78 @@ public class NoteRecorder {
     <uses-permission android:name="android.permission.RECORD_AUDIO" />
     <uses-permission android:name="android.permission.INTERNET" />
     <uses-permission android:name="android.permission.WAKE_LOCK" />
+    <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
+
+
+    <uses-feature android:name="android.hardware.camera"
+                  android:required="true" />
+
+                  <provider
+        android:name="android.support.v4.content.FileProvider"
+        android:authorities="com.example.android.fileprovider"
+        android:exported="false"
+        android:grantUriPermissions="true">
+        <meta-data
+            android:name="android.support.FILE_PROVIDER_PATHS"
+            android:resource="@xml/file_paths"></meta-data>
+    </provider>
+
 
 
     */
-MediaRecorder mediaRecorder;
+MediaRecorder audioRecorder;
+ProgressDialog progressDialog;
+
 MediaPlayer audioPlayer, videoPlayer;
 Uri test;
+String currentPhotoPath;
+
+
+
+
+static final int REQUEST_IMAGE_CAPTURE = 1;
 
 SharedPreferences sharedPreferences;
 
-public void recordAudio() throws IOException {
+public void recordAudio(String fileName) throws IOException {
     //Mangler noget der gemmet noten med samme navn som selve noten..
+    ContentValues values = new ContentValues(3);
+    //fileName = et eller andet navn på noten
+    values.put(MediaStore.MediaColumns.TITLE, fileName);
+    audioRecorder.setAudioSource(MicrophoneDirection.MIC_DIRECTION_TOWARDS_USER);
+    audioRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_2_TS);
+    audioRecorder.setAudioEncoder(DEFAULT);
+    audioRecorder.setOutputFile("/sdcard/sound/vikingesejllog" + fileName);
+    audioRecorder.prepare();
 
-    mediaRecorder.setAudioSource(MicrophoneDirection.MIC_DIRECTION_TOWARDS_USER);
-    mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_2_TS);
-    mediaRecorder.setOutputFile(test.getPath());
-    mediaRecorder.setAudioEncoder(AAC);
-    mediaRecorder.prepare();
-    mediaRecorder.start();
+    progressDialog.setTitle("Optager lydnote");
+    progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+    progressDialog.setButton("Afslut optagelse", new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            progressDialog.dismiss();
+            audioRecorder.stop();
+            audioRecorder.release();
+        }
+    });
+    progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+        @Override
+        public void onCancel(DialogInterface dialog) {
+            audioRecorder.stop();
+            audioRecorder.release();
+        }
+    });
+    audioRecorder.start();
+    progressDialog.show();
 }
 
-public void saveAudio(){
-    mediaRecorder.stop();
-    mediaRecorder.release();
-}
+/*public void saveAudio(){
+    audioRecorder.stop();
+    audioRecorder.release();
+}*/
 
 
-public void playAudioNote() throws IOException {
+public void playAudio() throws IOException {
     //Skal også have noget, der afspiller den rigtige lydfil afhængig af navn på note.
 
     audioPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
@@ -65,11 +121,48 @@ public void stopAudioNote(){
     audioPlayer.release();
 }
 
-public void saveImageNote(){
 
-}
+
+
+    private File saveImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+public void takePicture(){
+    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File imageNote = null;
+        try {
+            imageNote = saveImageFile();
+        } catch (IOException ex) {
+            System.out.println("Failed to save image");
+        }
+
+        // Continue only if the File was successfully created
+        if (imageNote != null) {
+            Uri imageUri = FileProvider.getUriForFile(this,
+                    "com.example.android.fileprovider",
+                    imageNote);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+        }
+    }
+
+
+
 
 public void showImageNote(){
-
+//Et eller andet med at klikke på det nye imageview og det åbner op.
 }
 }
