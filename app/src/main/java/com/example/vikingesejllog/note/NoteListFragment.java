14a71e9f1e0944fcc2095cb2ec0987b0e197f9cf
fmoltke <freggi64@live.dk>
2019-11-28 11:27:20 +0100

    Påbegyndt recyclerview til note list

--- app/src/main/java/com/example/vikingesejllog/note/NoteListFragment.java
+++ app/src/main/java/com/example/vikingesejllog/note/NoteListFragment.java
@@ -8,9 +8,13 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.vikingesejllog.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NoteListFragment extends Fragment {
 
@@ -18,12 +22,32 @@ public class NoteListFragment extends Fragment {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
 
+    private List<NoteListItem> noteListItems;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup view = (ViewGroup) inflater.inflate(R.layout.note_list_fragment, container, false);
 
-       // recyclerView = (RecyclerView) findViewById ???
+       recyclerView = (RecyclerView) view.findViewById(R.id.noteRecyclerView);
+       recyclerView.setHasFixedSize(true);
+       recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        noteListItems = new ArrayList<>();
+
+        int pencilID = getResources().getIdentifier("notePencil","drawable", getActivity().getPackageName());
+        int cameraID = getResources().getIdentifier("noteCamera","drawable", getActivity().getPackageName());
+        int micID = getResources().getIdentifier("noteMic","drawable", getActivity().getPackageName());
+
+        // Test list items
+        for (int i = 0; i<=10; i++){
+            NoteListItem noteListItem = new NoteListItem("Note " + (i+1),
+                    "28/10-2019 09:13", pencilID, cameraID, micID);
+            noteListItems.add(noteListItem);
+        }
+
+        adapter = new NoteListAdapter(noteListItems, getActivity());
+        recyclerView.setAdapter(adapter);
 
 
         return view;
