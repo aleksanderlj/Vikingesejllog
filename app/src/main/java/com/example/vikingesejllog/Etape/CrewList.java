39dd48416d48d05c7ea9c973dedb736037db5742
maximus <gammelgaard1993@hotmail.com>
2019-11-28 16:59:08 +0100

    Merge branch 'CrewList' of C:\Users\gamme\AndroidStudioProjects\Vikingesejllog with conflicts.

--- app/src/main/java/com/example/vikingesejllog/Etape/CrewList.java
+++ app/src/main/java/com/example/vikingesejllog/Etape/CrewList.java
@@ -1,26 +1,14 @@
 package com.example.vikingesejllog.Etape;
 
-import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.widget.Button;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.vikingesejllog.R;
-
-import java.util.ArrayList;
+import android.os.Bundle;
 
 public class CrewList extends AppCompatActivity {
 
-    ArrayList<String> crew;
-    Button crewCountButton;
-
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-
-        crewCountButton = findViewById(R.id.crewCountButton);
-
-
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.crew_list);
     }
 }
---
+++ app/src/main/java/com/example/vikingesejllog/Etape/CrewListItem.java
@@ -0,0 +1,4 @@
+package com.example.vikingesejllog.Etape;
+
+public class CrewListItem {
+}
