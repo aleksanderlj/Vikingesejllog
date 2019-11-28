946d47d0805e1da6c5efac33bd4a2ccf4fc6e271
maximus <gammelgaard1993@hotmail.com>
2019-11-28 16:59:24 +0100

    crew list item er lavet, alt andet mangler ..

--- app/src/main/java/com/example/vikingesejllog/Etape/CrewList.java
+++ app/src/main/java/com/example/vikingesejllog/Etape/CrewList.java
@@ -1,14 +1,26 @@
 package com.example.vikingesejllog.Etape;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.vikingesejllog.R;
+
 public class CrewList extends AppCompatActivity {
+
+    //private List<CrewListItem> crewListItems;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.crew_list);
+
+        //crewListItems = new ArrayList<>();
+
+
     }
 }
--- app/src/main/java/com/example/vikingesejllog/Etape/CrewListItem.java
+++ app/src/main/java/com/example/vikingesejllog/Etape/CrewListItem.java
@@ -1,4 +1,17 @@
 package com.example.vikingesejllog.Etape;
 
 public class CrewListItem {
+    private String crewMemberName;
+
+   public CrewListItem(String crewMemberName){
+       this.crewMemberName = crewMemberName;
+   }
+
+    public String getCrewMemberName() {
+        return crewMemberName;
+    }
+
+    public void setCrewMemberName(String crewMemberName) {
+        this.crewMemberName = crewMemberName;
+    }
 }
