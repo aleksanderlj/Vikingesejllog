07a3b61a81acc5322cfa9b9b448a5874d583e54f
Jonatan <Jonatandahl99@gmail.com>
2019-11-28 13:12:11 +0100

    Fixed indents

--- app/src/main/java/com/example/vikingesejllog/journey/NewJourney.java
+++ app/src/main/java/com/example/vikingesejllog/journey/NewJourney.java
@@ -6,40 +6,39 @@ import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.vikingesejllog.R;
 import com.example.vikingesejllog.model.Togt;
 import com.google.gson.Gson;
 
 public class NewJourney extends AppCompatActivity implements View.OnClickListener {
-    private TextView start, end;
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.new_journey);
-        start = findViewById(R.id.start);
-        end = findViewById(R.id.end);
-        findViewById(R.id.godkend).setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.godkend){
-            Togt togt;
-            String startPoint, endPoint;
-            startPoint = start.getText().toString();
-            endPoint = end.getText().toString();
-            togt = new Togt(startPoint, endPoint);
-            
-            Gson gson = new Gson();
-            String jsonTogt = gson.toJson(togt);
-            SharedPreferences prefs = getSharedPreferences("togtListe", MODE_PRIVATE);
-            SharedPreferences.Editor editor = getSharedPreferences("togtListe", MODE_PRIVATE).edit();
-            Integer hejsa = prefs.getAll().size() + 1;
-            editor.putString(hejsa.toString(), jsonTogt);
-            editor.apply();
-            
-            this.finish();
-        }
-    }
+	private TextView start, end;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.new_journey);
+		start = findViewById(R.id.start);
+		end = findViewById(R.id.end);
+		findViewById(R.id.godkend).setOnClickListener(this);
+	}
+	
+	@Override
+	public void onClick(View v) {
+		Togt togt;
+		String startPoint, endPoint;
+		startPoint = start.getText().toString();
+		endPoint = end.getText().toString();
+		togt = new Togt(startPoint, endPoint);
+		
+		Gson gson = new Gson();
+		String jsonTogt = gson.toJson(togt);
+		SharedPreferences prefs = getSharedPreferences("togtListe", MODE_PRIVATE);
+		SharedPreferences.Editor editor = getSharedPreferences("togtListe", MODE_PRIVATE).edit();
+		Integer hejsa = prefs.getAll().size() + 1;
+		editor.putString(hejsa.toString(), jsonTogt);
+		editor.apply();
+		
+		this.finish();
+	}
 }
