5fa2713331cba86c96d7e8c0cf462c165b6e5a95
Jonatan <Jonatandahl99@gmail.com>
2019-11-28 10:53:06 +0100

    Started work on create togt.

--- app/src/main/java/com/example/vikingesejllog/journey/NewJourney.java
+++ app/src/main/java/com/example/vikingesejllog/journey/NewJourney.java
@@ -3,22 +3,27 @@ package com.example.vikingesejllog.journey;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.vikingesejllog.R;
 
 public class NewJourney extends AppCompatActivity implements View.OnClickListener {
-
+    TextView start, end;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_journey);
-        findViewById(R.id.button11).setOnClickListener(this);
+        start = findViewById(R.id.start);
+        end = findViewById(R.id.end);
+        findViewById(R.id.godkend).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.button11:
+            case R.id.godkend:
                 this.finish();
                 break;
         }
