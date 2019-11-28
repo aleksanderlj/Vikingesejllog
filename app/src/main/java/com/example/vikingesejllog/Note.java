2cba707792c4348ebeaed7dc3f2a814631b7493c
DovendyretS <37805682+DovendyretS@users.noreply.github.com>
2019-11-28 12:36:34 +0100

    added Note class

---
+++ app/src/main/java/com/example/vikingesejllog/Note.java
@@ -0,0 +1,89 @@
+package com.example.vikingesejllog;
+
+public class Note {
+
+    private String gpsLoc;
+    private String boatSpeed;
+    private String windSpeed;
+    private String time;
+    private int rowers;
+    private String sailForing;
+    private String sailStilling;
+    private String course;
+
+    public Note(String gpsLoc, String boatSpeed, String windSpeed, String time, int rowers, String sailForing, String sailStilling, String course) {
+        this.gpsLoc = gpsLoc;
+        this.boatSpeed = boatSpeed;
+        this.windSpeed = windSpeed;
+        this.time = time;
+        this.rowers = rowers;
+        this.sailForing = sailForing;
+        this.sailStilling = sailStilling;
+        this.course = course;
+    }
+
+    public String getGpsLoc() {
+        return gpsLoc;
+    }
+
+    public void setGpsLoc(String gpsLoc) {
+        this.gpsLoc = gpsLoc;
+    }
+
+    public String getBoatSpeed() {
+        return boatSpeed;
+    }
+
+    public void setBoatSpeed(String boatSpeed) {
+        this.boatSpeed = boatSpeed;
+    }
+
+    public String getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(String windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getRowers() {
+        return rowers;
+    }
+
+    public void setRowers(int rowers) {
+        this.rowers = rowers;
+    }
+
+    public String getSailForing() {
+        return sailForing;
+    }
+
+    public void setSailForing(String sailForing) {
+        this.sailForing = sailForing;
+    }
+
+    public String getSailStilling() {
+        return sailStilling;
+    }
+
+    public void setSailStilling(String sailStilling) {
+        this.sailStilling = sailStilling;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+}
+
