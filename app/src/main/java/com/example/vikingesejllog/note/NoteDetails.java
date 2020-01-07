28bd5dda3a14679f3583c276d5587ebd2343ac7e
fmoltke <freggi64@live.dk>
2020-01-07 15:30:59 +0100

    Påbegyndt aktivitet til at vise detaljer for en note

---
+++ app/src/main/java/com/example/vikingesejllog/note/NoteDetails.java
@@ -0,0 +1,16 @@
+package com.example.vikingesejllog.note;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vikingesejllog.R;
+
+public class NoteDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_details);
+    }
+}
