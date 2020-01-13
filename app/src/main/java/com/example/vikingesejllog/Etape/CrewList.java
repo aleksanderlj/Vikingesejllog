3c966837a08ddc52270778966495efb801b3903e
maximus <gammelgaard1993@hotmail.com>
2020-01-13 14:25:32 +0100

    crew listener interface virker og kan sende information fra fragmentet til crewlist.

--- app/src/main/java/com/example/vikingesejllog/Etape/CrewList.java
+++ app/src/main/java/com/example/vikingesejllog/Etape/CrewList.java
@@ -1,6 +1,9 @@
 package com.example.vikingesejllog.etape;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -10,7 +13,13 @@ import com.example.vikingesejllog.R;
 
 import java.util.ArrayList;
 
-public class CrewList extends AppCompatActivity {
+public class CrewList extends AppCompatActivity implements View.OnClickListener, CrewListener {
+
+    private Button newCrewButton;
+    // Fragment
+    private AddCrewFragment addCrewFragment;
+    private EditText edittextCrewName;
+    private Button buttonConfirm;
 
     private RecyclerView recyclerView;
     private CrewListAdapter listAdapter;
@@ -21,6 +30,11 @@ public class CrewList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.note_activity_crewlist);
+
+        //fragment
+        addCrewFragment = new AddCrewFragment();
+
+        addCrewFragment.setCrewListener(this);
 
         crewListItems = new ArrayList<>();
 
@@ -37,6 +51,23 @@ public class CrewList extends AppCompatActivity {
         listAdapter = new CrewListAdapter(crewListItems,this);
         recyclerView.setAdapter(listAdapter);
 
+        newCrewButton = findViewById(R.id.newCrewButton);
+        newCrewButton.setOnClickListener(this);
+
+
     }
 
+    @Override
+    public void onClick(View v) {
+
+        if(v == newCrewButton){
+
+
+        }
+    }
+
+    @Override
+    public void onMemberSelected(String member) {
+        edittextCrewName.setText(member);
+    }
 }
