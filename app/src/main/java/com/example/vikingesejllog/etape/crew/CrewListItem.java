8d0eaa3f41564949a53b073996132255ada66e40
DovendyretS <37805682+DovendyretS@users.noreply.github.com>
2020-01-23 12:59:46 +0100

    re-added class gone missing xD

---
+++ app/src/main/java/com/example/vikingesejllog/etape/crew/CrewListItem.java
@@ -0,0 +1,18 @@
+package com.example.vikingesejllog.etape.crew;
+
+public class CrewListItem {
+
+    private String CrewMember;
+
+    public String getCrewMember() {
+        return CrewMember;
+    }
+
+    public void setCrewMember(String crewMember) {
+        CrewMember = crewMember;
+    }
+
+    public CrewListItem(String crewMember) {
+        CrewMember = crewMember;
+    }
+} 
